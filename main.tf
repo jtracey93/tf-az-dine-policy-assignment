0d5fb29e753692af24bf79210fa26012bc7f2f6c
Jack Tracey <41163455+jtracey93@users.noreply.github.com>
2021-02-01 21:08:51 +0000

    Add skip AAD check parameter

--- main.tf
+++ main.tf
@@ -26,8 +26,9 @@ resource "azurerm_policy_assignment" "dine-pol-asi" {
 resource "azurerm_role_assignment" "dine-pol-rbac-asi" {
   for_each = var.policyMsiRbacRoleNames
 
-  principal_id         = azurerm_policy_assignment.dine-pol-asi.identity[0].principal_id
-  scope                = var.assignmentScope
-  role_definition_name = each.value
+  principal_id                     = azurerm_policy_assignment.dine-pol-asi.identity[0].principal_id
+  scope                            = var.assignmentScope
+  role_definition_name             = each.value
+  skip_service_principal_aad_check = true
 }
 
